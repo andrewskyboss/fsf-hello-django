b766dc7a8d150fc32720000b1b40327bacb3373b
andrewskyboss <andrew.cybossky@gmail.com>
2021-09-01 22:56:01 +0000

    adding crud

--- django_new/urls.py
+++ django_new/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list
+# from todo.views import get_todo_list, add_item, edit_item
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name='get_todo_list')
+    path('', views.get_todo_list, name='get_todo_list'),
+    path('add', views.add_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name='edit'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
+    path('delete/<item_id>', views.delete_item, name='delete')
 ]
---
+++ todo/tests_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)
+
+
+    # def test_this_thing_works2(self):
+    #     self.assertEqual(1, 3)
+
+
+    # def test_this_thing_works3(self):
+    #     self.assertEqual(1, )
+
+
+    # def test_this_thing_works4(self):
+    #     self.assertEqual(1, 4)
